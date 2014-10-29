2a6a7d97e545c6ad34969a7d7647b865f447da24
Abdul Habra <abdul.habra@gmail.com>
2014-10-29 14:03:29 -0400

    added javadocs to Reducer

--- src/main/java/com/tek271/funj/Reducer.java
+++ src/main/java/com/tek271/funj/Reducer.java
@@ -1,16 +1,31 @@
 package com.tek271.funj;
 
+import com.google.common.annotations.Beta;
 import com.google.common.collect.Lists;
 
 import java.util.List;
 
 import static com.tek271.funj.CollectionTools.isEmpty;
 
+@Beta
 public class Reducer {
+
+	/**
+	 * interface to be implemented by reduce functions
+	 * @param <T> type of value to reduce.
+	 */
 	public static interface ReduceFunction<T> {
 		T reduceValue(T buffer, T value);
 	}
 
+	/**
+	 * Left reduce the given iterable into a single value using the given reduceFunction
+	 * @param iterable values to reduce starting at the first item
+	 * @param reduceFunction the function to apply on each item
+	 * @param initialValue the start value for the reduction
+	 * @param <T> Type of values in iterable
+	 * @return the reduced value
+	 */
 	public static <T> T reduce(Iterable<T> iterable,
 														 ReduceFunction<T> reduceFunction,
 														 T initialValue) {
@@ -23,6 +38,14 @@ public class Reducer {
 		return r;
 	}
 
+	/**
+	 * Right reduce the given iterable into a single value using the given reduceFunction
+	 * @param iterable values to reduce starting at the last item
+	 * @param reduceFunction the function to apply on each item
+	 * @param initialValue the start value for the reduction
+	 * @param <T> Type of values in iterable
+	 * @return the reduced value
+	 */
 	public static <T> T reduceRight(Iterable<T> iterable,
 														 ReduceFunction<T> reduceFunction,
 														 T initialValue) {
